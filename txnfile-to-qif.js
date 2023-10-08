588af7e46a4a93f61674a10117956f043802741b
russ <hartr.h16@gmail.com>
2023-10-08 15:36:38 -0400

    wip

--- txnfile-to-qif.js
+++ txnfile-to-qif.js
@@ -9,14 +9,66 @@ P${payee}
 `.trim();
 }
 
+function processFile(lines, fileType) {
+    let qifType, skipFirstLine;
+
+    if (fileType.toLowerCase() == "citizens") {
+        qifType = "Bank";
+        skipFirstLine = true;
+        reverseSign = false;
+    }
+    else if (fileType.toLowerCase() == "citizenscc") {
+        qifType = "CCard";
+        skipFirstLine = true;
+        reverseSign = true;
+    }
+
+    let text = `!Type:${qifType}\n`
+
+    lines.forEach(function (line, i) {
+        if (line.length > 0 && (!skipFirstLine || i > 0)) {
+            let date, payee, amount, reward;
+            let skipEntry = false;
+
+            if (fileType.toLowerCase() == "citizens") {
+                [, date, , payee, amount, , , ] = line.split(',').map(col => col.replace(/"/g,""));
+                
+                let dateObj = new Date(date);
+                date = `${dateObj.getMonth() + 1}/${dateObj.getDate()}/${dateObj.getFullYear()}`;
+
+                payee = payee.replace(/\s+/g," ");
+            }
+            else if (fileType.toLowerCase() == "citizenscc") {
+                line = "\"," + line + ",\"";  // so dumb
+                
+                [, date, , , , , , , payee, , , , , amount, reward, ] = line.split("\",\"").map(col => col.replace(/"/g,""));
+                
+                let [year, month, day] = date.split('-')
+                date = `${month}/${day}/${year}`;
+
+                amount = amount.replace("$","");
+
+                if (reward == '' && amount >= 0 && !payee.startsWith('INTEREST'))
+                {
+                    skipEntry = true;
+                }
+            }
+            
+            if (!skipEntry) {
+                if (reverseSign && !isNaN(amount)) amount = amount * -1;
+
+                text = text + qifEntry(date, amount, payee) + '\n';
+            }
+        }
+    });
+
+    return text;
+}
+
 module.exports = {
     main: function(fileType, inputFileName) {
-        if (!inputFileName.toLowerCase().endsWith(".csv")) {
-            console.log("ERROR: Requires CSV file");
-            return;
-        }
-
         console.log("QIF file generation started");
+        
         const fs = require('fs');
         const path = require('path');
 
@@ -26,58 +78,8 @@ module.exports = {
         let lines = fs
             .readFileSync(inputFileName, 'utf-8')
             .split('\n');
-        
-        let qifType, skipFirstLine;
 
-        if (fileType.toLowerCase() == "citizens") {
-            qifType = "Bank";
-            skipFirstLine = true;
-            reverseSign = false;
-        }
-        else if (fileType.toLowerCase() == "citizenscc") {
-            qifType = "CCard";
-            skipFirstLine = true;
-            reverseSign = true;
-        }
-
-        let text = `!Type:${qifType}\n`
-
-        lines.forEach(function (line, i) {
-            if (line.length > 0 && (!skipFirstLine || i > 0)) {
-                let date, payee, amount, reward;
-                let skipEntry = false;
-
-                if (fileType.toLowerCase() == "citizens") {
-                    [, date, , payee, amount, , , ] = line.split(',').map(col => col.replace(/"/g,""));
-                    
-                    let dateObj = new Date(date);
-                    date = `${dateObj.getMonth() + 1}/${dateObj.getDate()}/${dateObj.getFullYear()}`;
-
-                    payee = payee.replace(/\s+/g," ");
-                }
-                else if (fileType.toLowerCase() == "citizenscc") {
-					line = "\"," + line + ",\"";  // so dumb
-					
-                    [, date, , , , , , , payee, , , , , amount, reward, ] = line.split("\",\"").map(col => col.replace(/"/g,""));
-                    
-                    let [year, month, day] = date.split('-')
-                    date = `${month}/${day}/${year}`;
-
-                    amount = amount.replace("$","");
-
-                    if (reward == '' && amount >= 0 && !payee.startsWith('INTEREST'))
-                    {
-                        skipEntry = true;
-                    }
-                }
-                
-                if (!skipEntry) {
-                    if (reverseSign && !isNaN(amount)) amount = amount * -1;
-
-                    text = text + qifEntry(date, amount, payee) + '\n';
-                }
-            }
-        });
+        let text = processFile(lines, fileType);
 
         let outputPath = path.join(quickenFolder, `transactions-${fileType}.qif`);
 
